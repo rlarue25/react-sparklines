8e4d7146f1d2ef1b76a6768894d22ea2e92af1e6
rlarue <rlarue@chathamfinancial.com>
2022-06-28 15:43:00 -0400

    Updated SparklinesSpots to color min/max spots

--- src/SparklinesSpots.js
+++ src/SparklinesSpots.js
@@ -12,41 +12,40 @@ export default class SparklinesSpots extends React.Component {
     static defaultProps = {
         size: 2,
         spotColors: {
-            '-1': 'red',
-            '0': 'black',
-            '1': 'green'
+            'max': 'green',
+            'min': 'red'
         }
     };
 
-    lastDirection(points) {
-
-        Math.sign = Math.sign || function(x) { return x > 0 ? 1 : -1; }
-
-        return points.length < 2
-            ? 0
-            : Math.sign(points[points.length - 2].y - points[points.length - 1].y);
+    getIndex(points) {
+        const temp = points.map(p => p.y)
+        const minIndex = temp.indexOf(Math.max(temp))
+        const maxIndex = temp.indexOf(Math.min(temp))
+        return {minIndex, maxIndex}
     }
 
     render() {
 
         const { points, width, height, size, style, spotColors } = this.props;
 
-        const startSpot = <circle
-                            cx={points[0].x}
-                            cy={points[0].y}
-                            r={size}
-                            style={style} />
+        const {minIndex, maxIndex} = this.getIndex(points)
 
-        const endSpot = <circle
-                            cx={points[points.length - 1].x}
-                            cy={points[points.length - 1].y}
+        const maxSpot = <circle
+                            cx={points[maxIndex].x}
+                            cy={points[maxIndex].y}
                             r={size}
-                            style={style || { fill: spotColors[this.lastDirection(points)] }} />
+                            style={{fill: spotColors['max']}} />
+
+        const minSpot = <circle
+                            cx={points[minIndex].x}
+                            cy={points[minIndex].y}
+                            r={size}
+                            style={{fill: spotColors['min']}} />
 
         return (
             <g>
-                {style && startSpot}
-                {endSpot}
+                {maxSpot}
+                {minSpot}
             </g>
         )
     }
