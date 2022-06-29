779cd7d9d29e4d0a37efa24f1c7e152f25edac6d
rlarue <rlarue@chathamfinancial.com>
2022-06-29 08:54:26 -0400

    Created new component

---
+++ src/SparklinesSpotsMinMax.js
@@ -0,0 +1,45 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+
+export default class SparklinesSpots extends React.Component {
+
+    static propTypes = {
+        size: PropTypes.number,
+        style: PropTypes.object,
+    };
+
+    static defaultProps = {
+        size: 2,
+    };
+
+    getIndex(data) {
+        const minIndex = data.indexOf(Math.min.apply(Math, data))
+        const maxIndex = data.indexOf(Math.max.apply(Math, data))
+        return {minIndex, maxIndex}
+    }
+
+    render() {
+
+        const { data, points, width, height, size, style, spotColors } = this.props;
+
+        const {minIndex, maxIndex} = this.getIndex(data)
+        const maxSpot = <circle
+                            cx={points[maxIndex].x}
+                            cy={points[maxIndex].y}
+                            r={size}
+                            style={{fill: 'green'}} />
+
+        const minSpot = <circle
+                            cx={points[minIndex].x}
+                            cy={points[minIndex].y}
+                            r={size}
+                            style={{fill: 'red'}} />
+
+        return (
+            <g>
+                {maxSpot}
+                {minSpot}
+            </g>
+        )
+    }
+}
