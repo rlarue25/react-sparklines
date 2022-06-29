0b919c600758d8f7badd395c2a533af5f6c86e42
rlarue <rlarue@chathamfinancial.com>
2022-06-29 09:40:16 -0400

    Exported new componetn

--- src/SparklinesSpotsMinMax.js
+++ src/SparklinesSpotsMinMax.js
@@ -1,7 +1,7 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 
-export default class SparklinesSpots extends React.Component {
+export default class SparklinesSpotsMinMax extends React.Component {
 
     static propTypes = {
         size: PropTypes.number,
